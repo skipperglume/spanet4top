e7f6f771d164fac50edf1c500205ffd0e6ffee8c
Timoshyn Denys <timoshyd@hpc-head.troja.mff.cuni.cz>
2024-03-29 12:56:44 +0100

    added shared methods

--- utils/sharedMethods.py
+++ utils/sharedMethods.py
@@ -1,16 +1,17 @@
 import os
 
-def displayFoundFiles(fPaths : list):
+def displayFoundFiles(fPaths : list) -> None:
     numFile = len(fPaths)
     if numFile > 0:
         print('==============================================================')
-        print(f'Looking in {numFile} files:')
+        print(f'Looking in folder:\n{os.path.split(fPaths[0])[0]}')
         print(f'Found {numFile} files:')
         print('==============================================================')
     else:
         print('==============================================================')
         print(f'No files found')
         print('==============================================================')
+        return
 
     for i in range(numFile):
         name = os.path.basename(fPaths[i])
@@ -18,4 +19,4 @@ def displayFoundFiles(fPaths : list):
             print(f'ERROR: {fPaths[i]} is a directory')
             exit(1)
         else:
-            print(name)
+            print('  ',name)
